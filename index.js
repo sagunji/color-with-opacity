2fae2ecab748f092973bb0a59b9d156da87adb39
sagunji <karanjit.sagun01@gmail.com>
2022-07-20 15:59:25 -0400

    Add complete functions

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 /**
+ * Consumes a hex color and opacity value and returns a hex value mixing the opacity for a new solid color
+ *
  *
  * @param color : HEX color value
+ * @param opacity : 0-1 value
  * @returns
  */
-function colorWithoutOpacity(color) {
+function getSolidColor(color, opacity = 0.6) {
   const deriveHex = color.replace("#", "").split(/(?=(?:..)*$)/);
-  const a = 0.6;
-  // @ts-ignore
-  const r = Math.floor(`0x${deriveHex[0]}` * a + 0xff * (1 - a));
-  // @ts-ignore
-  const g = Math.floor(`0x${deriveHex[1]}` * a + 0xff * (1 - a));
-  // @ts-ignore
-  const b = Math.floor(`0x${deriveHex[2]}` * a + 0xff * (1 - a));
+  const r = Math.floor(`0x${deriveHex[0]}` * opacity + 0xff * (1 - a));
+  const g = Math.floor(`0x${deriveHex[1]}` * opacity + 0xff * (1 - a));
+  const b = Math.floor(`0x${deriveHex[2]}` * opacity + 0xff * (1 - a));
 
   return "#" + ((r << 16) | (g << 8) | b).toString(16);
 }
 
-export default colorWithoutOpacity;
+export default getSolidColor;
